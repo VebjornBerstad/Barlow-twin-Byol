593a69084eb1364e932fc436c55452b7bd2dffbf
Afterstone <invisiblex@gmail.com>
2023-05-14 12:09:44 +0000

    Remove unused dataset code

--- src/swarm/dataset.py
+++ src/swarm/dataset.py
@@ -3,13 +3,6 @@ import os
 import torch
 from torch.utils.data import Dataset
 
-# import random
-# import torch.nn.functional as F
-# import torchaudio as TA
-# from .conv_wav_mel import preprocess_audio
-# from torchaudio.transforms import Resample
-# from tqdm import tqdm
-
 
 class AudioDataset(Dataset):
     def __init__(self, main_dir, target_sample_rate, unit_sec, transform=None):
@@ -31,31 +24,9 @@ class AudioDataset(Dataset):
         return len(self.file_list)
 
     def __getitem__(self, idx):
-        # waveform, sample_rate = TA.load(self.file_list[idx])
         x = torch.load(self.file_list[idx])
         if x.ndim == 2:
             x = x.unsqueeze(0)
-
-        # if waveform.shape[0] > 1:
-        #     waveform = torch.mean(waveform, dim=0, keepdim=True)
-
-        # # Resample to target sample rate
-        # if sample_rate != self.target_sample_rate:
-        #     resampler = Resample(orig_freq=sample_rate, new_freq=self.target_sample_rate)
-        #     waveform = resampler(waveform)
-
-        # length_adj = self.unit_length - len(waveform)
-        # if length_adj > 0:
-        #     half_adj = int(length_adj // 2)
-        #     wav = F.pad(waveform, (half_adj, int(length_adj - half_adj)))
-
-        # length_adj = len(waveform) - self.unit_length
-        # start = random.randint(0, length_adj) if length_adj > 0 else 0
-        # waveform = waveform[start:start + self.unit_length]
-
-        # x = (self.to_mel_spec(wav) + torch.finfo().eps).log()
-
-        # log_mel_spectrogram = preprocess_audio(waveform=waveform, sample_rate=sample_rate)
 
         if self.transform:
             x = self.transform(x)
@@ -77,32 +48,9 @@ class AudiosetDataset(Dataset):
         return len(self.file_list)
 
     def __getitem__(self, idx):
-        # Load audio file
-        # waveform, sample_rate = TA.load(self.file_list[idx])
         x = torch.load(self.file_list[idx])
         if x.ndim == 2:
             x = x.unsqueeze(0)
-
-        # if waveform.shape[0] > 1:
-        #     waveform = torch.mean(waveform, dim=0, keepdim=True)
-
-        # # Resample to target sample rate
-        # if sample_rate != self.target_sample_rate:
-        #     resampler = Resample(orig_freq=sample_rate, new_freq=self.target_sample_rate)
-        #     waveform = resampler(waveform)
-
-        # length_adj = self.unit_length - len(waveform)
-        # if length_adj > 0:
-        #     half_adj = int(length_adj // 2)
-        #     wav = F.pad(waveform, (half_adj, int(length_adj - half_adj)))
-
-        # length_adj = len(waveform) - self.unit_length
-        # start = random.randint(0, length_adj) if length_adj > 0 else 0
-        # waveform = waveform[start:start + self.unit_length]
-
-        # x = (self.to_mel_spec(wav) + torch.finfo().eps).log()
-
-        # log_mel_spectrogram = preprocess_audio(waveform=waveform, sample_rate=sample_rate)
 
         if self.transform:
             x = self.transform(x)
