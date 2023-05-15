3756ebbd8aec6ccc6e4cc0c0d516171ee607f840
Afterstone <invisiblex@gmail.com>
2023-05-15 19:24:16 +0000

    Update documentation for losses.py

--- src/swarm/losses.py
+++ src/swarm/losses.py
@@ -6,14 +6,14 @@ import torch as T
 
 class CrossCorrelationLoss(T.nn.Module):
     """Cross correlation loss, as defined in the Barlow Twins paper.
-
-    Ref Algorithm 1 from:
-    J. Zbontar, L. Jing, I. Misra, Y. LeCun, and S. Deny,
-        ‘Barlow Twins: Self-Supervised Learning via Redundancy Reduction’,
-        2021, doi: 10.48550/ARXIV.2103.03230.
     """
 
     def __init__(self, lambda_: float = 5e-3, eps: float = 1e-12):
+        """
+        Args:
+            lambda_: The lambda parameter for the cross correlation loss.
+            eps: Epsilon used for numeric stability.
+        """
         super().__init__()
 
         self.lambda_ = lambda_
@@ -22,6 +22,9 @@ class CrossCorrelationLoss(T.nn.Module):
     def off_diagonal_ele(self, x: T.Tensor):
         """
         Returns the off-diagonal elements of a square matrix.
+
+        Ref. implementation from:
+        https://github.com/jonahanton/SSL_audio/blob/master/utils/utils.py
 
         Args:
             x: A square matrix.
@@ -33,7 +36,21 @@ class CrossCorrelationLoss(T.nn.Module):
         assert n == m
         return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
 
-    def forward(self, z1, z2):
+    def forward(self, z1: T.Tensor, z2: T.Tensor):
+        """Calculates the cross-correlation loss between the two input tensors z1 and z2.
+
+        Ref. Algorithm 1 from:
+        J. Zbontar, L. Jing, I. Misra, Y. LeCun, and S. Deny,
+            ‘Barlow Twins: Self-Supervised Learning via Redundancy Reduction’,
+            2021, doi: 10.48550/ARXIV.2103.03230.
+
+        Args:
+            z1: The first embedding tensor.
+            z2: The second embedding tensor.
+
+        Returns:
+            The cross-correlation loss between the two input tensors.
+        """
         assert z1.shape == z2.shape
 
         # normalize repr. along the batch dimension
