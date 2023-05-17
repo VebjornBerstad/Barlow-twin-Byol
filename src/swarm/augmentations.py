1dc839097d21401375add7d87dcca0d7f972f243
Afterstone <invisiblex@gmail.com>
2023-05-17 12:41:22 +0000

    Ad documentation for augmentations.py

--- src/swarm/augmentations.py
+++ src/swarm/augmentations.py
@@ -1,66 +1,124 @@
 import random
 
 import numpy as np
-import torch
-import torch.nn as nn
+import torch as T
 import torch.nn.functional as F
 
 
-class RandomCropWidth(nn.Module):
-    def __init__(self, target_frames):
+class RandomCropWidth(T.nn.Module):
+    """Randomly crop the input spectrogram to a fixed width."""
+
+    def __init__(self, target_frames: int):
+        """
+        Args:
+            target_frames: The target number of frames.
+        """
         super().__init__()
         self.target_frames = target_frames
 
-    def forward(self, log_mel_spectrogram):
-        num_frames = log_mel_spectrogram.shape[-1]
+    def forward(self, spectrogram: T.Tensor) -> T.Tensor:
+        """Randomly crop the input spectrogram to a fixed width.
+
+        Args:
+            spectrogram: The input spectrogram.
+
+        Returns:
+            A cropped spectrogram
+        """
+        num_frames = spectrogram.shape[-1]
         if num_frames > self.target_frames:
-            start = torch.randint(num_frames - self.target_frames + 1, (1,)).item()
-            log_mel_spectrogram = log_mel_spectrogram[:, :, start:start + self.target_frames]
+            start = T.randint(num_frames - self.target_frames + 1, (1,)).item()
+            spectrogram = spectrogram[:, :, start:start + self.target_frames]
         elif num_frames < self.target_frames:
-            padding = torch.zeros(1, 64, self.target_frames - num_frames)
-            log_mel_spectrogram = torch.cat((log_mel_spectrogram, padding), dim=-1)
-        return log_mel_spectrogram
+            padding = T.zeros(1, 64, self.target_frames - num_frames)
+            spectrogram = T.cat((spectrogram, padding), dim=-1)
+        return spectrogram
 
 
-class Normalize(nn.Module):
-    def __init__(self, eps=1e-8):
+class Normalize(T.nn.Module):
+    """Normalization of an input tensor along the channel dimension using a weighted mean and average."""
+
+    def __init__(self, eps: float = 1e-8):
+        """
+        Args:
+            eps: A small value to avoid division by zero.
+        """
         super(Normalize, self).__init__()
         self.eps = eps
 
-        self.mean: torch.Tensor = torch.tensor([0.0], requires_grad=False)
-        self.std: torch.Tensor = torch.tensor([1.0], requires_grad=False)
+        self.mean: T.Tensor = T.tensor([0.0], requires_grad=False)
+        self.std: T.Tensor = T.tensor([1.0], requires_grad=False)
         self.examples_seen: int = 0
 
-    def forward(self, x):
+    def forward(self, x: T.Tensor) -> T.Tensor:
+        """Normalize the input tensor along the channel dimension.
+
+        Args:
+            x: The input tensor.
+
+        Returns:
+            The normalized input tensor.
+        """
+
+        # TODO: Consider using weights instead of directly multiplying by self.examples_seen and batch_size.
+
         batch_size = x.shape[0]
 
-        batch_mean = torch.mean(x, dim=(0, 2, 3), keepdim=True)
+        batch_mean = T.mean(x, dim=(0, 2, 3), keepdim=True)
         self.mean = self.mean.to(x.device)
         self.mean = (self.mean * self.examples_seen + batch_mean * batch_size) / (self.examples_seen + batch_size)
 
-        batch_std = torch.std(x, dim=(0, 2, 3), unbiased=False, keepdim=True).to(x.device) + self.eps
+        batch_std = T.std(x, dim=(0, 2, 3), unbiased=False, keepdim=True).to(x.device) + self.eps
         self.std = self.std.to(x.device)
         self.std = (self.std * self.examples_seen + batch_std * batch_size) / (self.examples_seen + batch_size)
 
         self.examples_seen += batch_size
 
-        return (x - self.mean) / self.std
+        return (x - self.mean) / (self.std + self.eps)
 
 
-class RandomLinearFader(nn.Module):
-    def __init__(self, gain=1.0):
+class RandomLinearFader(T.nn.Module):
+    """Randomly add a linear fade to the input spectrogram.
+
+    Idea and implementation from:
+    [1] D. Niizumi, D. Takeuchi, Y. Ohishi, N. Harada, and K. Kashino, ‘BYOL for Audio: Exploring Pre-Trained General-Purpose Audio Representations’,
+        IEEE/ACM Transactions on Audio, Speech, and Language Processing, vol. 31, pp. 137–151, 2023, doi: 10.1109/TASLP.2022.3221007.
+    """
+
+    def __init__(self, gain: float = 1.0):
+        """
+        Args:
+            gain: The gain of the linear fading.
+        """
         super().__init__()
         self.gain = gain
 
-    def forward(self, lms):
+    def forward(self, spectrogram: T.Tensor) -> T.Tensor:
+        """Randomly add a linear fade to the input spectrogram.
+
+        Args:
+            spectrogram: The input spectrogram.
+
+        Returns:
+            The spectrogram with a linear fade applied.
+        """
         head, tail = self.gain * ((2.0 * np.random.rand(2)) - 1.0)
-        T = lms.shape[1]
-        slope = torch.linspace(head, tail, T, dtype=lms.dtype).reshape(1, T).to(lms.device)
-        y = lms + slope
+        t = spectrogram.shape[1]
+        slope = T.linspace(head, tail, t, dtype=spectrogram.dtype).reshape(1, t).to(spectrogram.device)
+        y = spectrogram + slope
         return y
 
 
-class RandomResizeCrop(nn.Module):
+class RandomResizeCrop(T.nn.Module):
+    """Randomly crop the input spectrogram to a fixed width.
+
+    Idea and implementation from:
+    [1] D. Niizumi, D. Takeuchi, Y. Ohishi, N. Harada, and K. Kashino, ‘BYOL for Audio: Exploring Pre-Trained General-Purpose Audio Representations’,
+        IEEE/ACM Transactions on Audio, Speech, and Language Processing, vol. 31, pp. 137–151, 2023, doi: 10.1109/TASLP.2022.3221007.
+    [2] D. Niizumi, D. Takeuchi, Y. Ohishi, N. Harada, and K. Kashino, ‘BYOL for Audio: Self-Supervised Learning for General-Purpose Audio Representation’,
+        in 2021 International Joint Conference on Neural Networks (IJCNN), Jul. 2021, pp. 1–8. doi: 10.1109/IJCNN52387.2021.9534474.
+    """
+
     def __init__(self, virtual_crop_scale=(1.0, 1.5), freq_scale=(0.6, 1.5), time_scale=(0.6, 1.5)):
         super().__init__()
         assert time_scale[1] >= 1.0
@@ -71,7 +129,22 @@ class RandomResizeCrop(nn.Module):
         self.interpolation = 'bicubic'
 
     @staticmethod
-    def get_params(virtual_crop_size, in_size, time_scale, freq_scale):
+    def get_params(
+        virtual_crop_size: tuple[float, float],
+        in_size: tuple[int, int],
+        time_scale: tuple[float, float],
+        freq_scale: tuple[float, float]
+    ):
+        """Sample parameters to use for a random resize crop operation.
+
+        Args:
+            virtual_crop_size: The size of the virtual crop.
+            in_size: The size of the input spectrogram.
+            time_scale: The range of the time scale.
+            freq_scale: The range of the frequency scale.
+        Returns:
+            The parameters to use for the random resize crop operation.
+        """
         canvas_h, canvas_w = virtual_crop_size
         src_h, src_w = in_size
         h = np.clip(int(np.random.uniform(*freq_scale) * src_h), 1, canvas_h)
@@ -80,10 +153,18 @@ class RandomResizeCrop(nn.Module):
         j = random.randint(0, canvas_w - w) if canvas_w > w else 0
         return i, j, h, w
 
-    def forward(self, lms):
+    def forward(self, lms: T.Tensor) -> T.Tensor:
+        """Randomly crop the input spectrogram to a fixed width.
+
+        Args:
+            lms: An input log-mel spectrogram.
+
+        Returns:
+            The cropped spectrogram.
+        """
         virtual_crop_size = [int(s * c) for s, c in zip(lms.shape[-2:], self.virtual_crop_scale)]
-        virtual_crop_area = (torch.zeros((lms.shape[0], lms.shape[1], virtual_crop_size[0], virtual_crop_size[1]))
-                             .to(torch.float).to(lms.device))
+        virtual_crop_area = (T.zeros((lms.shape[0], lms.shape[1], virtual_crop_size[0], virtual_crop_size[1]))
+                             .to(T.float).to(lms.device))
         lh, lw = virtual_crop_area.shape[-2:]
         h, w = lms.shape[-2:]
         x, y = (lw - w) // 2, (lh - h) // 2
@@ -92,46 +173,71 @@ class RandomResizeCrop(nn.Module):
         crop = virtual_crop_area[:, :, i:i+h, j:j+w]
         lms = F.interpolate(crop, size=lms.shape[-2:],
                             mode=self.interpolation, align_corners=True)
-        return lms.to(torch.float)
+        return lms.to(T.float)
 
 
-def log_mixup_exp(xa, xb, alpha):
+def log_mixup_exp(xa: T.Tensor, xb: T.Tensor, alpha: float):
+    """Mix up two tensors in log-scale.
+
+    Args:
+        xa: The first tensor.
+        xb: The second tensor.
+        alpha: The mixing ratio.
+
+    Returns:
+        The mixed tensor.
+    """
     xa = xa.exp()
     xb = xb.exp()
     x = alpha * xa + (1. - alpha) * xb
-    return torch.log(x + torch.finfo(x.dtype).eps)
+    return T.log(x + T.finfo(x.dtype).eps)
 
 
-class MixupBYOLA(nn.Module):
-    """Mixup for BYOL-A.
-    Args:
-        ratio: Alpha in the paper.
-        n_memory: Size of memory bank FIFO.
-        log_mixup_exp: Use log-mixup-exp to mix if this is True, or mix without notion of log-scale.
+class MixupBYOLA(T.nn.Module):
+    """Simulates adding background noise to the input spectrogram, to separate the foreground (what to learn) from the background (augmentation noise).
+
+    Idea and implementation from:
+    [1] D. Niizumi, D. Takeuchi, Y. Ohishi, N. Harada, and K. Kashino, ‘BYOL for Audio: Exploring Pre-Trained General-Purpose Audio Representations’,
+        IEEE/ACM Transactions on Audio, Speech, and Language Processing, vol. 31, pp. 137–151, 2023, doi: 10.1109/TASLP.2022.3221007.
+    [2] D. Niizumi, D. Takeuchi, Y. Ohishi, N. Harada, and K. Kashino, ‘BYOL for Audio: Self-Supervised Learning for General-Purpose Audio Representation’,
+        in 2021 International Joint Conference on Neural Networks (IJCNN), Jul. 2021, pp. 1–8. doi: 10.1109/IJCNN52387.2021.9534474.
     """
 
-    def __init__(self, ratio: float, n_memory: int, log_mixup_exp=True):
+    def __init__(self, ratio: float, n_memory: int, log_mixup_exp: bool = True):
+        """
+        Args:
+            ratio: Alpha in the paper.
+            n_memory: Size of memory bank FIFO.
+            log_mixup_exp: Use log-mixup-exp to mix if this is True, or mix without notion of log-scale.
+        """
         super().__init__()
         self.ratio = ratio
         self.n = n_memory
         self.log_mixup_exp = log_mixup_exp
         self.memory_bank = []
 
-    def forward(self, x):
+    def forward(self, spectrogram: T.Tensor):
+        """Apply mixup to the input spectrogram.
+
+        Args:
+            spectrogram: The input spectrogram.
+        Returns:
+            The mixed spectrogram.
+        """
         # mix random
         alpha = self.ratio * np.random.random()
         if self.memory_bank:
             # get z as a mixing background sound
-            z = self.memory_bank[np.random.randint(len(self.memory_bank))].to(x.device)
+            z = self.memory_bank[np.random.randint(len(self.memory_bank))].to(spectrogram.device)
             # mix them
-            mixed = log_mixup_exp(x, z, 1. - alpha) if self.log_mixup_exp \
-                else alpha * z + (1. - alpha) * x
+            mixed = log_mixup_exp(spectrogram, z, 1. - alpha) if self.log_mixup_exp \
+                else alpha * z + (1. - alpha) * spectrogram
         else:
-            mixed = x
+            mixed = spectrogram
         # update memory bank
-        self.memory_bank = (self.memory_bank + [x.cpu()])[-self.n:]
+        self.memory_bank = (self.memory_bank + [spectrogram.cpu()])[-self.n:]
 
-        return mixed.to(torch.float)
+        return mixed.to(T.float)
 
 
 def aug_pipeline(
@@ -145,8 +251,24 @@ def aug_pipeline(
     rrc_time_scale_min: float = 0.6,
     rrc_time_scale_max: float = 1.5,
 ):
-    return nn.Sequential(
-        Normalize(),
+    """Create a pipeline of augmentations to use for training using default augmentations.
+
+    Args:
+        mixup_ratio: The ratio of mixup to use.
+        mixup_memory_size: The size of the memory bank to use for mixup.
+        linear_fader_gain: The gain of the linear fader.
+        rrc_crop_scale_min: The minimum crop scale to use for random resize crop.
+        rrc_crop_scale_max: The maximum crop scale to use for random resize crop.
+        rrc_freq_scale_min: The minimum frequency scale to use for random resize crop.
+        rrc_freq_scale_max: The maximum frequency scale to use for random resize crop.
+        rrc_time_scale_min: The minimum time scale to use for random resize crop.
+        rrc_time_scale_max: The maximum time scale to use for random resize crop.
+
+    Returns:
+        A pipeline of augmentations to use for training.
+    """
+    return T.nn.Sequential(
+        Normalize(),  # Pre-normalization
         MixupBYOLA(ratio=mixup_ratio, n_memory=mixup_memory_size),
         RandomResizeCrop(
             virtual_crop_scale=(rrc_crop_scale_min, rrc_crop_scale_max),
@@ -156,5 +278,5 @@ def aug_pipeline(
         RandomLinearFader(
             gain=linear_fader_gain,
         ),
-        Normalize(),
+        Normalize(),  # Post-normalization
     )
